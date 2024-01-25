cb1b1dc8a9dedf99be0640d6021b17520f9e3e70
Banaashree <banashreegogoi19@gmail.com>
2024-01-25 18:54:54 +0000

    variables

---
+++ Basics/01variable.js
@@ -0,0 +1,10 @@
+const accountId =145222;
+let accountEmail ="banashree@google.com"
+var accountPassword = 15451;
+accountCity="Guwahati"
+//accountId = 154;
+accountEmail = "kuldeep@gmail.com"
+accountPassword =122;
+accountCity ="Bangalore"
+// console.log(accountEmail)
+console.table([accountId,accountEmail,accountPassword,accountCity]);
