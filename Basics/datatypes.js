d04edfb060694a53a59b4ebc8952e17394aa75f6
Banaashree <banashreegogoi19@gmail.com>
2024-01-26 05:48:12 +0000

    datatypes

---
+++ Basics/datatypes.js
@@ -0,0 +1,24 @@
+"use strict";
+
+// alert(3+3)// we are using node js, not browser
+
+// console.log(3
+    
+//     + 3); //code readabiliy should be high
+
+    let name="Banashree"
+    let age = 26;
+    let isLoggedin =false;
+
+//Primitive Datatypes
+    //number
+    //bigint
+    //string =>""
+    //boolean=>true/false
+    //null= standalone value
+    //undefined=>
+    //symbol=>unique
+
+//Object
+console.log(typeof undefined); //undefined
+console.log(typeof null); //object
